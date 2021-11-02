a81fb4499d79411bc48e6e65178f4f6705ac28f8
Onionie <gutierrez.onionie@gmail.com>
2021-11-01 22:26:02 -0700

    Navbar styling working properly

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -6,15 +6,15 @@ import "./NavLink.css";
 function NavLink() {
   return (
     <div>
-      <Navbar className="header" bg="light" expand="lg">
-        <div>
+      <Navbar className="header" bg="dark" expand="lg">
+        <div className="title">
           <Link to="/">
             <BeachAccessIcon className="svg" /> TripPH
           </Link>
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link className="ltext" as={Link} to="/hotels">
               Hotels
             </Nav.Link>
