0b78666640e13210271bd6449d4132970a6b3ba2
Onionie <gutierrez.onionie@gmail.com>
2021-11-02 23:07:29 -0700

    Added Card outline color

--- src/components/AdventureComp.js
+++ src/components/AdventureComp.js
@@ -2,13 +2,13 @@ import { Card } from "react-bootstrap";
 
 function AdventureComp() {
   return (
-    <Card style={{ width: "25rem" }}>
+    <Card border="info" style={{ width: "25rem" }}>
       <Card.Img
         variant="top"
         src="https://a.cdn-hotels.com/gdcs/production87/d1048/4ed1a1e8-afa7-4640-b8ca-d1d6ccbb54c1.jpg?impolicy=fcrop&w=1600&h=1066&q=medium"
       />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>El Nido Marine Reserve Park</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
