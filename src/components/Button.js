9a859031982dd4932410f82944d71c3fffdbd731
Onionie <gutierrez.onionie@gmail.com>
2021-11-03 19:14:26 -0700

    Working on Button Functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,14 @@
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import { Link } from "react-router-dom";
 import "./Button.css";
 
-function Button() {
+function Button(props) {
+  const clickHandler = () => {
+    <Link to={props.} ></Link>
+  };
+
   return (
-    <button>
+    <button onClick={clickHandler}>
       <ArrowForwardIcon />
     </button>
   );
