6fb82d4da6f72427f35a11f32745bb963238ff7b
Onionie <gutierrez.onionie@gmail.com>
2021-11-04 21:46:28 -0700

    Added content in Restaurant

--- src/components/RestaurantComp.js
+++ src/components/RestaurantComp.js
@@ -9,9 +9,19 @@ function RestaurantComp() {
       <h1>RESTAURANT</h1>
       <Row className="style" xs={1} md={2} lg={3}>
         <CompCard
-          imgSrc="https://a.cdn-hotels.com/gdcs/production87/d1048/4ed1a1e8-afa7-4640-b8ca-d1d6ccbb54c1.jpg?impolicy=fcrop&w=1600&h=1066&q=medium"
-          title="El Nido Marine Reserve Park"
-          cText="This Marine Reserve Park is popular for its limestone cliffs, 50 beaches, 3 major marine habitats, 5 types of forest, and an abundant fauna and flora including over 20 endemic species."
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/09/ka-lui-restaurant-300x205.jpg"
+          title="Kalui Restaurant"
+          cText="KaLui is a native-style restaurant popular with tourists and locals for its fresh and affordable seafood. It’s the most famous restaurant in Puerto Princesa.  Fresh catch of the day, includes sashimi, tempura, shellfish soup, baked mussel and much more."
+        />
+        <CompCard
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/La_terrace_Palawan_rest-300x205.jpg"
+          title="La Terrasse Restaurant"
+          cText="A stylish restaurant in Puerto Princesa that serves amazing dishes made with fresh and organic ingredients. La Terrace Restaurant is the ideal venue for a romantic dinner in Puerto Princesa. The varied menu is based on high-quality local produce.."
+        />
+        <CompCard
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/tiya_ising_food_1-300x205.jpg"
+          title="Tiya Ising’s Filipino Restaurant"
+          cText="Tiya Ising is a restaurant serving traditional Filipino comfort food. The interior is modern-retro style, filled with old photographs of Palawan, and antiques. Tiya Ising is the best place to go if you are looking for a restaurant that serves Filipino cuisine in Palawan."
         />
       </Row>
     </div>
