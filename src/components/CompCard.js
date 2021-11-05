c3fb24f969f1e01b87508268dc94713857794768
Onionie <gutierrez.onionie@gmail.com>
2021-11-04 20:40:24 -0700

    Remove unnecessary prop

--- src/components/CompCard.js
+++ src/components/CompCard.js
@@ -4,7 +4,7 @@ function CompCard(props) {
   return (
     <Col>
       <Card border="info" style={{ width: "25rem", marginTop: "5%" }}>
-        <Card.Img variant={props.bsVariant} src={props.imgSrc} />
+        <Card.Img variant="top" src={props.imgSrc} />
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>{props.cText}</Card.Text>
