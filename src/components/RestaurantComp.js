2f68eceb05fe22b725391d649492d033d4eb8908
Onionie <gutierrez.onionie@gmail.com>
2021-11-04 22:16:12 -0700

    Added more Restaurant content

--- src/components/RestaurantComp.js
+++ src/components/RestaurantComp.js
@@ -16,12 +16,27 @@ function RestaurantComp() {
         <CompCard
           imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/La_terrace_Palawan_rest-300x205.jpg"
           title="La Terrasse Restaurant"
-          cText="A stylish restaurant in Puerto Princesa that serves amazing dishes made with fresh and organic ingredients. La Terrace Restaurant is the ideal venue for a romantic dinner in Puerto Princesa. The varied menu is based on high-quality local produce.."
+          cText="A stylish restaurant in Puerto Princesa that serves amazing dishes made with fresh and organic ingredients. La Terrace Restaurant is the ideal venue for a romantic dinner in Puerto Princesa. The varied menu is based on high-quality local produce."
         />
         <CompCard
           imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/tiya_ising_food_1-300x205.jpg"
           title="Tiya Ising’s Filipino Restaurant"
-          cText="Tiya Ising is a restaurant serving traditional Filipino comfort food. The interior is modern-retro style, filled with old photographs of Palawan, and antiques. Tiya Ising is the best place to go if you are looking for a restaurant that serves Filipino cuisine in Palawan."
+          cText="Tiya Ising is a restaurant serving traditional Filipino comfort food. Tiya Ising is the best place to go if you are looking for a restaurant that serves Filipino cuisine in Palawan."
+        />
+        <CompCard
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/09/ka-joel-inato-300x205.jpg"
+          title="Ka Joel’s Inato Restaurant"
+          cText="Another native-style restaurant with a great variety of food choice from poultry, beef, pork, and seafood. Ka Inato Restaurant serves delicious Filipino and native food at an affordable price. The most popular are the grilled chicken leg served with steamed rice."
+        />
+        <CompCard
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/09/puerto-vista-restaurant-romatic-dinner-300x205.jpg"
+          title="Puerto Vista Restaurant"
+          cText="The only restaurant in Puerto Princesa which offers Wagyu Beef. Wagyu cattle is known worldwide for its marbling characteristics and naturally enhanced flavor, tenderness, and juiciness. Puerto Vista also caters delicious French-Spanish and Moroccan cuisines. "
+        />
+        <CompCard
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/09/haim-chicken-inato-300x205.jpg"
+          title="Haim Chicken Inato Restaurant"
+          cText="Offers more than just chicken! It has an extensive menu to satisfy every other kind of craving. The bestseller is, of course, chicken, grilled to perfection. Those who have tried it would attest that it’s grilled chicken like no other, especially when paired with rice, which is essential in a common Filipino meal."
         />
       </Row>
     </div>
