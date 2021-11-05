d9430bf1a63370c8c7b105670c6d775b0889f417
Onionie <gutierrez.onionie@gmail.com>
2021-11-04 22:27:56 -0700

    Finished Restaurant page

--- src/components/RestaurantComp.js
+++ src/components/RestaurantComp.js
@@ -19,9 +19,9 @@ function RestaurantComp() {
           cText="A stylish restaurant in Puerto Princesa that serves amazing dishes made with fresh and organic ingredients. La Terrace Restaurant is the ideal venue for a romantic dinner in Puerto Princesa. The varied menu is based on high-quality local produce."
         />
         <CompCard
-          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/tiya_ising_food_1-300x205.jpg"
-          title="Tiya Ising’s Filipino Restaurant"
-          cText="Tiya Ising is a restaurant serving traditional Filipino comfort food. Tiya Ising is the best place to go if you are looking for a restaurant that serves Filipino cuisine in Palawan."
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/kinabuch-grill-and-bar-300x205.jpg"
+          title="Kinabuchs Grill and Bar"
+          cText="Kinabuch Bar and Grill is one of the most popular restaurants in Puerto Princesa with a wide selection of local, international and exotic food."
         />
         <CompCard
           imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/09/ka-joel-inato-300x205.jpg"
@@ -37,6 +37,21 @@ function RestaurantComp() {
           imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/09/haim-chicken-inato-300x205.jpg"
           title="Haim Chicken Inato Restaurant"
           cText="Offers more than just chicken! It has an extensive menu to satisfy every other kind of craving. The bestseller is, of course, chicken, grilled to perfection. Those who have tried it would attest that it’s grilled chicken like no other, especially when paired with rice, which is essential in a common Filipino meal."
+        />
+        <CompCard
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/pizza_is-300x205.jpg"
+          title="Mc Coy’s Pizza House Restaurant"
+          cText="If you’re craving for Italian food in Puerto Princesa then there is no better place to go then McCoy’s Pizza House. It’s one of the most popular restaurants among locals in Puerto Princesa with great food and live music."
+        />
+        <CompCard
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/badjao_food-300x205.jpg"
+          title="Badjao Seafront Restaurant"
+          cText="The Badjao Seafront is a restaurant built on stilts above the clear waters and surrounded by a mangrove forest overlooking the ocean. Badjao’s sea and mountain views are truly mesmerizing. The Badjao Seafront Restaurant serves a variety of seafood and Filipino dishes."
+        />
+        <CompCard
+          imgSrc="https://www.travel-palawan.com/wp-content/uploads/2018/02/tiya_ising_food_1-300x205.jpg"
+          title="Tiya Ising’s Filipino Restaurant"
+          cText="Tiya Ising is a restaurant serving traditional Filipino comfort food. Tiya Ising is the best place to go if you are looking for a restaurant that serves Filipino cuisine in Palawan."
         />
       </Row>
     </div>
