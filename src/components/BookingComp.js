c34e8b2f54047e5641b722a805ded33768a08526
Onionie <gutierrez.onionie@gmail.com>
2021-11-07 23:10:28 -0800

    Working on Booking Comp

---
+++ src/components/BookingComp.js
@@ -0,0 +1,15 @@
+import CompCard from "./CompCard";
+import { Row } from "react-bootstrap";
+import "./Comp.css";
+import AdventureData from "../data/AdventurePageData.json";
+
+function AdventureComp() {
+  return (
+    <div className="comp">
+      <h1>BOOKING</h1>
+      <Row className="style" xs={1} md={2} lg={3}></Row>
+    </div>
+  );
+}
+
+export default AdventureComp;
---
+++ src/pages/Booking.js
@@ -0,0 +1,13 @@
+import BookingComp from "../components/BookingComp";
+import ScrollToTopOnMount from "../components/ScrollToTopMount";
+
+function Booking() {
+  return (
+    <div>
+      <ScrollToTopOnMount />
+      <BookingComp />
+    </div>
+  );
+}
+
+export default Booking;
