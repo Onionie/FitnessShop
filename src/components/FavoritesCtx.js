796b31263120fc0f7cefecf622891d417f1b2227
Onionie <gutierrez.onionie@gmail.com>
2021-11-20 18:02:18 -0800

    Implementing Context

---
+++ src/components/FavoritesCtx.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+//Initial values
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavoirtes] = useState([]);
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
