740c47f147fcc5c898b2550a968ddf8199971e41
Onionie <gutierrez.onionie@gmail.com>
2021-11-20 18:17:39 -0800

    Added function handlers

--- src/components/FavoritesCtx.js
+++ src/components/FavoritesCtx.js
@@ -7,7 +7,26 @@ const FavoritesContext = createContext({
 });
 
 function FavoritesContextProvider(props) {
-  const [userFavorites, setUserFavoirtes] = useState([]);
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  /* favoriteMeetup parameter will be added to initial state prevUserFavorites 
+  that then will be the new state setUserFavorites */
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorite) => {
+      return prevUserFavorite.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites((prevUserFavorite) => {
+      //Filter method: looks for a meetup where meetup.id is not equal to meetupID
+      return prevUserFavorite.filter((meetup) => meetup.id !== meetupId);
+    });
+  }
+
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id !== meetupId);
+  }
 
   const context = {
     favorites: userFavorites,
