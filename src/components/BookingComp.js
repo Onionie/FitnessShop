513c23cfb5e1214c19c80a8168def253b9dd9938
Onionie <gutierrez.onionie@gmail.com>
2021-11-20 23:55:27 -0800

    Succesfully Renders a Booking

--- src/components/BookingComp.js
+++ src/components/BookingComp.js
@@ -1,10 +1,29 @@
+import { useContext } from "react";
+import { Row } from "react-bootstrap";
 import "./Comp.css";
+import CompCard from "./CompCard";
+import FavoritesContext from "./FavoritesCtx";
 
 function BookingComp() {
+  const favoritesCtx = useContext(FavoritesContext);
   return (
-    <div className="comp">
-      <h1>BOOKING</h1>
-    </div>
+    <section>
+      <div className="comp">
+        <h1>BOOKING</h1>
+        <Row className="style" xs={1} md={2} lg={3}>
+          {/* Using .map function to map through our .json data to make code cleaner */}
+          {favoritesCtx.favorites.map((item) => (
+            <CompCard
+              key={item.id}
+              id={item.id}
+              imgSrc={item.image}
+              title={item.title}
+              cText={item.description}
+            />
+          ))}
+        </Row>
+      </div>
+    </section>
   );
 }
 
