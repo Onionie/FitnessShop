0e6effcef630b064c848fbb31c6eafbbd69d3688
Onionie <gutierrez.onionie@gmail.com>
2021-12-10 19:22:43 -0800

    Add h3 when booking is empty

--- src/components/BookingComp.js
+++ src/components/BookingComp.js
@@ -6,22 +6,37 @@ import FavoritesContext from "./FavoritesCtx";
 
 function BookingComp() {
   const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = (
+      <div className="centerText">
+        <h3> You have not booked anything yet. Start adding some?</h3>
+      </div>
+    );
+  } else {
+    content = (
+      <Row className="style" xs={1} md={2} lg={3}>
+        {/* Using .map function to map through our .json data to make code cleaner */}
+        {favoritesCtx.favorites.map((item) => (
+          <CompCard
+            key={item.id}
+            id={item.id}
+            imgSrc={item.image}
+            title={item.title}
+            cText={item.description}
+          />
+        ))}
+      </Row>
+    );
+  }
+
   return (
     <section>
       <div className="comp">
         <h1>BOOKING</h1>
-        <Row className="style" xs={1} md={2} lg={3}>
-          {/* Using .map function to map through our .json data to make code cleaner */}
-          {favoritesCtx.favorites.map((item) => (
-            <CompCard
-              key={item.id}
-              id={item.id}
-              imgSrc={item.image}
-              title={item.title}
-              cText={item.description}
-            />
-          ))}
-        </Row>
+        {content}
       </div>
     </section>
   );
