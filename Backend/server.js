1cbfdeae2840647f43b3c218f6860c61d9ff29d6
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-02-05 23:01:13 -0800

    Retrieves api data for Supplements

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const accessoriesProducts = require('./data/AccessoriesProducts.js');
 const equipmentProducts = require('./data/EquipmentProducts.js');
+const supplementProducts = require('./data/SupplementsProducts.js');
 
 app.get('/', (req, res) => {
   res.send('API is running...');
@@ -12,19 +13,31 @@ app.get('/api/accessories', (req, res) => {
   res.json(accessoriesProducts);
 });
 
-app.get('/api/equipment', (req, res) => {
-  res.json(equipmentProducts);
-});
-
 // Shows specific product
 app.get('/api/accessories/:id', (req, res) => {
   const accProduct = accessoriesProducts.find((p) => p._id === req.params.id);
   res.json(accProduct);
 });
 
+// Equipment Data
+app.get('/api/equipment', (req, res) => {
+  res.json(equipmentProducts);
+});
+
 app.get('/api/equipment/:id', (req, res) => {
   const eqqProduct = equipmentProducts.find((p) => p._id === req.params.id);
   res.json(eqqProduct);
+});
+
+// Supplement Data
+app.get('/api/supplements', (req, res) => {
+  res.json(supplementProducts);
+});
+
+// Shows specific supplement product
+app.get('/api/supplements/:id', (req, res) => {
+  const suppProduct = supplementProducts.find((p) => p._id === req.params.id);
+  res.json(suppProduct);
 });
 
 app.listen(5000, console.log('Server is running on port 5000'));
