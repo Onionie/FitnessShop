ea16e65131d2e3803fbd0d9a23a06851541de037
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-02-05 22:57:09 -0800

    Retrieves data api for equipment products

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const accessoriesProducts = require('./data/AccessoriesProducts.js');
+const equipmentProducts = require('./data/EquipmentProducts.js');
 
 app.get('/', (req, res) => {
   res.send('API is running...');
@@ -11,10 +12,19 @@ app.get('/api/accessories', (req, res) => {
   res.json(accessoriesProducts);
 });
 
+app.get('/api/equipment', (req, res) => {
+  res.json(equipmentProducts);
+});
+
 // Shows specific product
 app.get('/api/accessories/:id', (req, res) => {
   const accProduct = accessoriesProducts.find((p) => p._id === req.params.id);
   res.json(accProduct);
+});
+
+app.get('/api/equipment/:id', (req, res) => {
+  const eqqProduct = equipmentProducts.find((p) => p._id === req.params.id);
+  res.json(eqqProduct);
 });
 
 app.listen(5000, console.log('Server is running on port 5000'));
