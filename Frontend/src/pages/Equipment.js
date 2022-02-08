0500b74be46e4aafe9df5b0e6ff40bb49d1fd9d9
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-02-07 22:14:16 -0800

    Fetch equipment data from backend

--- Frontend/src/pages/Equipment.js
+++ Frontend/src/pages/Equipment.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import ProductsPage from '../components/ProductsPage';
 //import equipmentProducts from '../data/EquipmentProducts';
-import allProducts from '../data/allProducts';
+import axios from 'axios';
 
 const Equipment = () => {
-  const equipmentProducts = allProducts.filter(
-    (p) => p.category === 'equipment'
-  );
-  console.log(equipmentProducts);
+  const [equipmentProducts, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      const newEquipments = data.filter((p) => p.category === 'equipment');
+
+      setProducts(newEquipments);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <Row>
       {equipmentProducts.map((product) => (
-        <Col sm={12} md={6} lg={4} xl={4}>
+        <Col sm={12} md={6} lg={4} xl={4} key={product._id}>
           <ProductsPage product={product} />
         </Col>
       ))}
