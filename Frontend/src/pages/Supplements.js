68573c173ad7b7f3c22deb592b9e647ed1c20e26
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-02-08 16:53:58 -0800

    Supplement data fetch from backend

--- Frontend/src/pages/Supplements.js
+++ Frontend/src/pages/Supplements.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import ProductsPage from '../components/ProductsPage';
-import supplementsProducts from '../data/SupplementsProducts';
+import axios from 'axios';
 
 const Supplement = () => {
+  const [supplementProducts, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      const newSupplements = data.filter((p) => p.category === 'supplement');
+
+      setProducts(newSupplements);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <Row>
-      {supplementsProducts.map((product) => (
+      {supplementProducts.map((product) => (
         <Col sm={12} md={6} lg={4} xl={4}>
           <ProductsPage product={product} />
         </Col>
