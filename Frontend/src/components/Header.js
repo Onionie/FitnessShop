9b44f9d101496207936b83f4dee0176215deb65a
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-02-09 22:36:22 -0800

    Removed text underline from Title

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import '../pages/pages.css';
 
 const Header = () => {
   return (
