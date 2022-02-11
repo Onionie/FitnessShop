5dace36d6e12fef321110b55ecb6371dd4576965
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-02-10 23:59:34 -0800

    Fix Quantity initial State

--- Frontend/src/pages/SpecificProduct.js
+++ Frontend/src/pages/SpecificProduct.js
@@ -18,7 +18,7 @@ import { listProductDetails } from '../actions/productActions';
 import './pages.css';
 
 const SpecificProduct = () => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const navigate = useNavigate();
   const params = useParams();
