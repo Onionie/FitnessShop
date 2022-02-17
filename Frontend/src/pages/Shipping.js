1c022b287f9b9b1825747aefe4a6195a45545287
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-02-16 23:12:23 -0800

    Implementend Checkout steps

--- Frontend/src/pages/Shipping.js
+++ Frontend/src/pages/Shipping.js
@@ -4,7 +4,7 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from '../actions/cartActions';
-// import CheckoutSteps from '../components/CheckoutSteps';
+import CheckoutSteps from '../components/CheckoutSteps';
 
 const Shipping = () => {
   const cart = useSelector((state) => state.cart);
@@ -41,7 +41,7 @@ const Shipping = () => {
 
   return (
     <FormContainer>
-      {/* <CheckoutSteps step1 step2 /> */}
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
