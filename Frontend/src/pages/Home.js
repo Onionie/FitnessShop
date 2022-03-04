04265ce5a18f751b3b657ae06e982e1ea7bbfd42
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-03-03 20:04:37 -0800

    Added Header-image

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,11 +1,29 @@
-import { Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 import './pages.css';
 
 function Home() {
   return (
-    <div>
-      <div className="background-banner"></div>
-    </div>
+    <>
+      <div className="background-banner">
+        <Container className="row" style={{ marginTop: '0' }}>
+          <Row>
+            <Col className="header-text" md={12} lg={6}>
+              Text about page
+            </Col>
+            <Col md={12} lg={6}>
+              <Image
+                src="./images/homePage/header-image.png"
+                className="header-image"
+                fluid
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Container>
+        <h1>Something</h1>
+      </Container>
+    </>
   );
 }
 
